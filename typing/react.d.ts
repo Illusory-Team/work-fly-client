87469c1a019acaee97c0a6e64f63c50421489303
OddEer0 <OddEer0@gmail.com>
2022-12-31 20:02:25 +0600

    refactor/864dkzdra fix lint

--- typing/react.d.ts
+++ typing/react.d.ts
@@ -1,15 +1,14 @@
 declare module '*.svg' {
-  import * as React from 'react';
+	import * as React from 'react';
 
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> 
-  >;
+	export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
 
-  const src: string;
-  export default src;
+	const src: string;
+	export default src;
 }
 
 declare module '*.png' {
-  const content: any;
-  export default content;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const content: any;
+	export default content;
 }
