4f26db182364f698255756c1e9eddba90d115f05
OddEer0 <OddEer0@gmail.com>
2023-01-16 21:47:24 +0600

    [feature/861m7v09y] add Avatar component

---
+++ src/components/ui/avatars/Avatar/Avatar.types.ts
@@ -0,0 +1,12 @@
+import { HTMLAttributes } from 'react';
+
+type VariantType = 'circle' | 'square' | 'rounded';
+type SizeType = 'l' | 'm' | 's' | 'xs';
+
+export interface AvatarProps extends HTMLAttributes<HTMLDivElement> {
+	className?: string;
+	variant?: VariantType;
+	src?: string;
+	alt?: string;
+	size?: SizeType;
+}
