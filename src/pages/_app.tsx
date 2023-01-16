82a0a4d483e3e7793dfdacf87903516e3b831369
edword03 <dima_veretennikov@list.ru>
2023-01-16 23:21:18 +0400

    [feature/861m7wm69]: integrate react-query and fix github workflow script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,27 @@
 import type { AppProps } from 'next/app';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import '@/styles/index.scss';
 import { wrapper } from '@/store/store';
 import { Provider } from 'react-redux';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 export default function App({ Component, ...rest }: AppProps) {
 	const { store, props } = wrapper.useWrappedStore(rest);
 
 	return (
-		<Provider store={store}>
-			<Component {...props.pageProps} />
-		</Provider>
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<Component {...props.pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+			</Provider>
+		</QueryClientProvider>
 	);
 }
