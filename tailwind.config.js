584c13a67d5ea30f328fb07fb98db2402c32941d
Leahim <128914772+Leahim2085@users.noreply.github.com>
2023-04-18 21:48:16 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const plugin = require('tailwindcss/plugin');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	content: ['./src/**/*.{ts,tsx}'],
 	theme: {
 		extend: {
 			transitionTimingFunction: {
