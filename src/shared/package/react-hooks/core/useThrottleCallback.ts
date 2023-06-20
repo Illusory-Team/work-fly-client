8fc0c686905b5fa23e70b9a0b2a4d1af436d29d7
OddEer0 <OddEer0@gmail.com>
2023-06-20 22:39:56 +0600

    [migration/WF-149]: debugging

--- src/shared/package/react-hooks/core/useThrottleCallback.ts
+++ src/shared/package/react-hooks/core/useThrottleCallback.ts
@@ -1,13 +1,13 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 
 export const useThrottleCallback = (callback: () => void, delayValue: number) => {
 	const delay = useRef(true);
 
-	return (): void => {
+	return useCallback((): void => {
 		if (delay.current) {
 			callback();
 			delay.current = false;
 			setTimeout(() => (delay.current = true), delayValue);
 		}
-	};
+	}, []);
 };
