d2d97b18fa0af3fac8d8e364e64148ac525ebcbb
OddEer0 <OddEer0@gmail.com>
2023-07-16 16:41:42 +0300

    [test/WF-175]: debugging

--- src/shared/package/react-hooks/core/useMediaQuery.ts
+++ src/shared/package/react-hooks/core/useMediaQuery.ts
@@ -32,7 +32,6 @@ export const useMediaQuery = (query: string): boolean => {
 				matchMedia.removeEventListener('change', handleChange);
 			}
 		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [query]);
 
 	return matches;
