36449ab76563418eba1ed1bb90e5ad16b95b116c
edword03 <dima_veretennikov@list.ru>
2023-07-28 18:28:48 +0400

    [refactor/WF-154]: refactored services

--- src/shared/api/services/auth.service.ts
+++ src/shared/api/services/auth.service.ts
@@ -1,26 +1,22 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
-import { setCookie } from 'cookies-next';
-import { NextPageContext } from 'next';
+import axios, { AxiosInstance } from 'axios';
 
-import { LOGIN_PATH, isAuthPath } from '@/shared/config/paths';
-import { redirectSSR } from '@/shared/config/redirectSSR';
 import { errorCatch } from '@/shared/lib/helpers/errorCatch';
 import { logger } from '@/shared/lib/helpers/logger';
 
-import { fakeApi } from '../instance';
+import { api } from '../instance';
 import { userResponseMapper } from '../mappers';
-import { AuthLoginRequest, AuthResponse, User } from '../types';
+import { AccessTokenResponse, AuthLoginRequest, AuthResponse, User } from '../types';
 
 class AuthService {
 	api: AxiosInstance;
 	constructor() {
-		this.api = fakeApi;
+		this.api = api;
 	}
 
 	async login(loginData: AuthLoginRequest): Promise<User | undefined> {
 		try {
 			const { data } = await this.api.post<AuthResponse>('/auth/login', loginData);
-			this.api.defaults.headers.common['x-csrf-token'] = data.csrfToken;
+			this.api.defaults.headers.common['Authorization'] = `Bearer ${data.accessToken}`;
 
 			return userResponseMapper(data);
 		} catch (error: unknown) {
@@ -30,47 +26,23 @@ class AuthService {
 		}
 	}
 
-	async refreshToken(ctx?: NextPageContext): Promise<AxiosResponse<User> | undefined> {
-		if (!!ctx) {
-			const { req, res, asPath } = ctx;
+	async refreshToken(): Promise<AccessTokenResponse | undefined> {
+		try {
+			const response = await this.api.get<AccessTokenResponse>('/auth/refresh');
 
-			try {
-				this.api.defaults.headers.cookie = req?.headers.cookie as string;
+			this.api.defaults.headers.common['Authorization'] = `Bearer ${response.data.accessToken}`;
 
-				const response = await this.api.get<User>('/auth/refresh', {
-					transformResponse: [userResponseMapper],
-				});
+			return response.data;
+		} catch (error: unknown) {
+			if (axios.isAxiosError(error)) {
+				logger({ type: 'error', message: errorCatch(error) });
 
-				if (response.headers['set-cookie']) {
-					const cookies = response.headers['set-cookie'];
-
-					cookies.forEach((cookieStr: string) => {
-						const [name, value] = cookieStr.split(';')[0].split('=');
-						const maxAge = cookieStr.split(';')[1].split('=')[1];
-
-						if (name === 'refreshToken' || name === 'accessToken') {
-							setCookie(name, value, { res, req, httpOnly: true, maxAge: Number(maxAge) });
-						}
-					});
+				if (error?.response?.data.message === 'jwt expired') {
 				}
 
-				return response;
-			} catch (error: unknown) {
-				if (axios.isAxiosError(error)) {
-					logger({ type: 'error', message: errorCatch(error) });
-
-					if (error?.response?.data.message === 'Unauthorized' && !isAuthPath(asPath)) {
-						redirectSSR(LOGIN_PATH, res);
-					}
-
-					return;
-				}
+				throw new Error(error.message);
 			}
 		}
-
-		return this.api.get<User>('/auth/refresh', {
-			transformResponse: [userResponseMapper],
-		});
 	}
 
 	async logout() {
