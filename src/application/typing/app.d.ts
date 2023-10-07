1537dc2fb4cf81c4a501733af5adf8fa186901c0
edword03 <dima_veretennikov@list.ru>
2023-10-08 01:39:22 +0400

    [migration/WOR-172_vite]: add config for app

--- src/application/typing/app.d.ts
+++ src/application/typing/app.d.ts
@@ -1,8 +1,14 @@
+import { IAppConfig } from '~/shared/config/appConfig';
+
 declare global {
 	export type Nullable<T> = T | null;
 
 	export type Keys<T extends Record<string, unknown>> = keyof T;
 	export type Values<T extends Record<string, unknown>> = T[Keys<T>];
+
+	interface Window {
+		APP_CONFIG: IAppConfig;
+	}
 }
 
 export {};
